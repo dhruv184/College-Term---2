6dd1f294839bf4b2fe3fbcc9fe37fcd85ef54918
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-11-10 09:48:06 -0500

    Started coddig in Day3.py

--- Day3.py
+++ Day3.py
@@ -1,4 +1,9 @@
 
-x = int(input("Enter a number : "))
-result = 10/x
-print(result)
+try:
+    x = int(input("Enter a number : "))
+    result = 10/x
+    print(result)
+except:
+    print("Error")
+
+print("done")
