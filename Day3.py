8b64d08affbecfec6a800856fabded59f7b67a38
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-11-10 09:58:47 -0500

    First code

--- Day3.py
+++ Day3.py
@@ -1,9 +1,27 @@
 
-try:
-    x = int(input("Enter a number : "))
-    result = 10/x
-    print(result)
-except:
-    print("Error")
+"""
+Exception Handling Example
+"""
+
+while True:
+    
+    try:
+        
+        x = int(input("Enter a number : "))
+        result = 10/x
+        print(result)
+        break
+
+    except ValueError as e:
+
+        print("Error" , e)
+
+    except ZeroDivisionError as ex:
+
+        print("Error 2" , ex)    
+
+    except Exception as e:
+
+        print("ERROR" , e)
 
 print("done")
