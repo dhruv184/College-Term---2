568c5e3d8c22575ce6898e34dcc2f22cd9852647
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-11-15 12:39:54 -0500

    Second code in Day4

--- Day4.py
+++ Day4.py
@@ -1,3 +1,4 @@
+'''
 from tkinter import *
 
 root = Tk()
@@ -14,3 +15,35 @@ label3 = Label(root , text = "Blue" , bg = "blue")
 label3.pack(side = LEFT , fill = BOTH)
 
 root.mainloop()
+'''
+#print("=======================")
+'''
+"""
+The app should allow the user to enter
+two numbers. when Submit
+button is clicked add the numbers
+and display the total. Using GUI.
+"""
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("GUI Example")
+root.geometry("500x600")
+
+for i in range(5):
+
+    label1 = tk.Label(root , text = "Red" , background = "red")
+    label1.pack(side = "left")
+
+    label2 = tk.Label(root , text = "Green" , background = "green")
+    label2.pack(side = "right")
+
+    label3 = tk.Label(root , text = "Yellow" , background = "yellow")
+    label3.pack(side = "top")
+
+    label4 = tk.Label(root , text = "Blue" , background = "blue")
+    label4.pack(side = "bottom")
+
+root.mainloop()
+'''
