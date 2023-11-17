84a1c0c108b1221eb6d18d5dd1302797747ab388
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-11-17 10:05:55 -0500

    2nd code in day5

--- Day5.py
+++ Day5.py
@@ -14,3 +14,57 @@ print(txt)
 root.mainloop()
 '''
 #print("=======================")
+
+import tkinter as tk
+
+from tkinter import ttk
+
+def action():
+
+    if option1.get() == "1":
+        print("option is selected")
+
+    elif size.get() == 1:
+        print("Size = Small")    
+    
+    elif size.get() == 2:
+        print("Size = Medium")
+
+    elif size.get() == 3:
+        print("Size = Large") 
+
+    else:
+        print("option 1 is not selected")    
+        
+
+root = tk.Tk()
+root.geometry("300x300")
+
+option1 = tk.StringVar()
+
+cb1 = ttk.Checkbutton(root, text = "Option 1" , variable = option1 , onvalue = 1 , offvalue = 0) 
+cb1.pack()
+
+label1 = ttk.Label(root, text = "Select Pizza Size")
+label1.pack()
+
+frame1 = tk.Frame(root)
+frame1.pack()
+
+size = tk.IntVar()
+
+rbtn1 = ttk.Radiobutton(frame1 , text = "Small " , variable = size , value = 1)
+rbtn1.pack(side = "left")
+
+rbtn2 = ttk.Radiobutton(frame1 , text = "Medium " , variable = size , value = 2)
+rbtn2.pack(side = "left")
+
+rbtn3 = ttk.Radiobutton(frame1 , text = "Large " , variable = size , value = 3)
+rbtn3.pack(side = "left")
+
+btn1 = ttk.Button(root , text = "Button 1" , command = action)
+btn1.pack()
+
+root.mainloop()
+
+#print("=======================")
