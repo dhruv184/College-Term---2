a76a206a241dfa30560c5d926598d062dfd53e35
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-11-22 12:37:39 -0500

    2nd code in day6

--- Day6.py
+++ Day6.py
@@ -22,3 +22,40 @@ fruits.append("Watermelon")
 print("\nModified list : ",fruits,"\n")
 '''
 #print("=======================")
+'''
+"""
+Access and manipulate Lists with loops
+Given the list: numbers = [2, 7, 1, 8, 3, 5, 11, 10, 6]
+Perform the following Operations using Loops
+1. Print the original list
+2. Print the square of each number in the list
+3. Calculate and print the sum of all the numbers in the list
+4. Create a new list of even numbers from the original list. Print the
+new list.
+5. Determine and print the maximum number in the list.
+"""
+numbers = [2, 7, 1, 8, 3, 5, 11, 10, 6]
+
+print("\nOriginal list : ",numbers)
+
+square = [ ]
+
+for num in numbers:
+    square.append(num ** 2)
+print("\nSquare of each number in the list : ", square)
+
+sum = 0
+for num in numbers:
+    sum += num
+print("\nThe sum of all the numbers in the list : ",sum)
+
+even = [ ]
+
+for num in numbers:
+    if num % 2 == 0:
+        even.append(num)
+print("\nNew list of even number : ", even) 
+
+print("\nMaximum number : ",max(numbers),"\n")
+'''
+#print("=======================")
