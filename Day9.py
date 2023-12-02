6b67ce76ba59c378eb554466bcc21e58229495ac
Dhruv Patel <dhruvpatel@dhruvs-air.home>
2023-12-02 10:44:59 -0500

    3rd code in day9

--- Day9.py
+++ Day9.py
@@ -17,4 +17,15 @@ with open('json_file_output.json' , 'w') as file:
 
     json.dump(d,file)
 '''    
-#print("=======================")   
+#print("=======================")
+'''
+import json
+
+with open('JsonFile.json' , 'r') as file:
+     data = json.load(file)
+     print("Name =", data['Name'] )
+     print("Age =", data['Age'])
+     print("Phone", data['Phone'][0])
+     print(type(data))
+'''
+#print("=======================")
