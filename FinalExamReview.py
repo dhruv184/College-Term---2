97a307a0e9f6fd271fce48d83d2812ea24f1147e
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-12-06 13:24:43 -0500

    1st code in FinalExam

--- FinalExamReview.py
+++ FinalExamReview.py
@@ -18,7 +18,7 @@ class Product:
 
     def __str__(self):
 
-        return f"Id : {self.id} , Name : {self.name} , Price : {self.price}"
+        return f"\nId : {self.id} , Name : {self.name} , Price : {self.price}"
 
 class Store:
 
@@ -33,6 +33,19 @@ class Store:
     def getProducts(self):
 
         return self.products
+    
+    def findProduct(self, id):
+
+        p = None
+
+        for product in self.products:
+
+            if product.id == id:
+
+                p = product
+                break
+        
+        return p
 
 store = Store()
 
@@ -40,23 +53,52 @@ def displayMainMenu():
 
     print("\nPlease select One of the following Options : \n")
 
-    print("1 to View the list of Products\n")
-    print("2 to Add Products\n")
+    print("1 to Add Products\n")
+    print("2 to View the list of Products\n")
     print("3 to Find Products\n")
     print("4 to Exit\n")
 
 while True :     
     
-    x = int(input(" ::-> \n"))
-
     displayMainMenu()
+
+    x = int(input(" :->: "))
     
     if x == 1 :
+        
+        print("\n")
+        id = input("Enter Product id : ")
+        name = input("Enter product Name : ")
+        price = input("Enter product price : ")
+
+        p = Product(id,name,price)
+
+        store.addProduct(p)
+
+    elif x == 2 :
 
         print("\nList of Products : \n")
 
         for p in store.getProducts():
 
+            print(p)    
+
+    elif x == 3 :
+        
+        print("\n")
+        id = input("Enter Product id to Find Product : ")
+
+        p = store.findProduct(id)
+
+        if isinstance(p , Product):
+
             print(p)
-    
-       
+
+        else:
+
+            print(f"Product with ID = {id} [ Not Found ] ")    
+
+    elif x == 4 :
+
+        print("\n Thanks You \n")
+        break        
