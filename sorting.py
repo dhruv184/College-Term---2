e433d3514aa5bce7b6b3a2efaf4ac28757c0e301
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-12-08 09:35:24 -0500

    Added code

--- sorting.py
+++ sorting.py
@@ -39,3 +39,23 @@ sorted_by_second_let = sorted(ex_lst , key = second_let)
 print(sorted_by_second_let)
 '''
 #print("=======================")
+'''
+"""
+Below, we have provided a list of strings called nums. 
+Write a function called last_char that takes a string 
+as input, and returns only its last character. 
+Use this function to sort the list nums by the last 
+digit of each number, from highest to lowest, and save
+this as a new list called nums_sorted.
+"""
+nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+
+def last_char(n):
+
+    size = len(n)
+    return n[size - 1]
+
+nums_sorted = sorted(nums , key = last_char , reverse = True)
+print(nums_sorted)
+'''
+#print("=======================")
