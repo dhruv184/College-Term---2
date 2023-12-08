e8a243619f8c562ebab6bcbf9a34cead1c1e4f6b
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-12-08 09:30:39 -0500

    Added code

--- sorting.py
+++ sorting.py
@@ -20,3 +20,22 @@ l3 = sorted(l1, reverse = True , key = absolute)
 print(l3)
 '''
 #print("=======================")
+'''
+"""
+You will be sorting the following list by each element’s 
+second letter, a to z.Create a function to use when sorting, 
+called second_let. It will take a string as input and return 
+the second letter of that string. Then sort the list, create
+a variable called sorted_by_second_let and assign the sorted 
+list to it.Do not use lambda.
+"""
+ex_lst = ['hi', 'how are you', 'bye', 'apple', 'zebra', 'dance']
+
+def second_let(e):
+
+    return e[1]
+
+sorted_by_second_let = sorted(ex_lst , key = second_let)
+print(sorted_by_second_let)
+'''
+#print("=======================")
