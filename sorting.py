62a3bc82a70e832f194ab2295fb6737a0a5fa5af
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-12-08 09:39:57 -0500

    Added code

--- sorting.py
+++ sorting.py
@@ -59,3 +59,16 @@ nums_sorted = sorted(nums , key = last_char , reverse = True)
 print(nums_sorted)
 '''
 #print("=======================")
+'''
+"""
+Once again, sort the list nums based on the last digit 
+of each number from highest to lowest. However, now you 
+should do so by writing a lambda function. Save the new 
+list as nums_sorted_lambda.
+"""
+nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+
+nums_sorted_lambda = sorted( nums , key = lambda n : n[len(n) - 1] , reverse = True )
+print(nums_sorted_lambda)
+'''
+#print("=======================")
