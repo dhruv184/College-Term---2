18a55af15e59d15be181872953c308cabb10b2f5
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-12-08 09:23:52 -0500

    Added code

--- sorting.py
+++ sorting.py
@@ -1,6 +1,22 @@
+'''
 l1 = [1,2,4,7,-3,-2]
 l1.sort()
 print(l1)
 
 l3 = sorted(l1, reverse = True)
 print(l3)
+'''
+#print("=======================")
+'''
+l1 = [1,2,4,7,-3,-2]
+l1.sort()
+print(l1)
+def absolute(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+l3 = sorted(l1, reverse = True , key = absolute)
+print(l3)
+'''
+#print("=======================")
